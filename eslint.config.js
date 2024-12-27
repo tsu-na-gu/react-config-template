e6fd611e6bcb580d648a71b14969b4dfabdb5b27
tsu-na-gu <skybluecyclist@gmail.com>
2024-12-28 06:23:32 +0900

    eslint.config.js change to flat file

--- eslint.config.js
+++ eslint.config.js
@@ -1,44 +1,51 @@
-export default tseslint.config(
-  { ignores: ['dist'] },
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import reactPlugin from 'eslint-plugin-react';
+
+export default [
   {
-    extends: [
-      js.configs.recommended,
-      ...tseslint.configs.recommended,
-      'plugin:react/recommended',
-      'plugin:import/recommended', 
-      'plugin:jsx-a11y/recommended',
-      'eslint-config-prettier'
-    ],
+    ignores: ['node_modules/', 'dist/', '.prettierrc.cjs', 'eslint.config.js', 'vite.config.ts'],
+  },
+  {
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parser: tsParser,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
     },
     plugins: {
+      '@typescript-eslint': tseslint,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      react: reactPlugin,
     },
     rules: {
+      ...tseslint.configs.recommended.rules, // 変更点
+      ...reactPlugin.configs.recommended.rules, // 変更点
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true }
-      ],
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
       '@typescript-eslint/no-explicit-any': 'error',
-      '@typescript-eslint/no-unnecessary-type-constraint': 'error'
+      '@typescript-eslint/no-unnecessary-type-constraint': 'error',
     },
     settings: {
       react: {
-        version: 'detect'
+        version: 'detect',
       },
       'import/resolver': {
         node: {
-          paths: ['src'],
-          extensions: ['.js', '.jsx', '.ts', '.tsx']
-        }
-      }
-    }
-  }
- )
- 
- 
+          paths: ['src/**/*'],
+          extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        },
+      },
+    },
+  },
+];
